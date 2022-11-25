3ca49eac65f7bfd68d328a2450ce665b8b7d3619
Mateusz Borowski <mateusz.borowski2.stud@pw.edu.pl>
2022-11-25 22:31:07 +0100

    Store mail password in secrets

--- mail_test.py
+++ mail_test.py
@@ -1,7 +1,8 @@
 from redmail import outlook
+from secrets import mail_password
 
 outlook.username = "czarny-lotos@outlook.com"
-outlook.password = ""
+outlook.password = mail_password
 
 outlook.send(
     subject="Test mail",
