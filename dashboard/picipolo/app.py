caeda34d5de33fabfe5cd337fa21353665d46c52
PawelGlr <01161422@pw.edu.pl>
2022-11-25 21:28:51 +0100

    fixed dashboard

--- dashboard/picipolo/app.py
+++ dashboard/picipolo/app.py
@@ -1,14 +1,13 @@
 import streamlit as st
 
 from multipage import MultiPage
-from pages import data_upload, heatmap, wordcloudpage, friends, messageranking
+from pages import heatmap, friends, messageranking
 
 # Create an instance of the app
 app = MultiPage()
 
 # Title of the main page
 st.title("Messenger and Facebook analysis")
-
 
 app.add_page("Messages", heatmap.app)
 app.add_page("Friends", friends.app)
