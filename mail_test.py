13cf4e370d08c2f880a3c627801f2d546ca72c2e
Mateusz Borowski <mateusz.borowski2.stud@pw.edu.pl>
2022-11-25 22:25:14 +0100

    Use redmail library

--- mail_test.py
+++ mail_test.py
@@ -1,18 +1,10 @@
-import smtplib
-# import ssl
-port = 587  
-smtp_server = "smtp-mail.outlook.com"
-sender = "czarny-lotos@outlook.com"
-recipient = "czarny-lotos@outlook.com"
-sender_password = ""
-message = """
-Subject: This is a test message
-Send using Python."""
-# SSL_context = ssl.create_default_context()
-# with smtplib.SMTP_SSL(smtp_server, port, context=SSL_context) as server:
-with smtplib.SMTP(smtp_server, port) as server:
+from redmail import outlook
 
-    server.ehlo()
-    server.starttls()
-    server.login(sender, sender_password)
-    server.sendmail(sender, recipient, message)
+outlook.username = "czarny-lotos@outlook.com"
+outlook.password = ""
+
+outlook.send(
+    subject="Test mail",
+    receivers=["czarny-lotos@outlook.com"],
+    text="Test text"
+)
