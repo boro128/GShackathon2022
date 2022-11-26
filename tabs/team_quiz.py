cb0fd5a9e24a2c2a45665301c80f3c8a83fee888
Szymon-Gut <72514490+Szymon-Gut@users.noreply.github.com>
2022-11-26 11:26:24 +0100

    quiz team calculate

--- tabs/team_quiz.py
+++ tabs/team_quiz.py
@@ -11,20 +11,49 @@ def load_questions(path):
 
 
 def show_team_1():
-    st.markdown("zakon chleba")
+    st.markdown("Data Analysis Team")
 
 
 def show_team_2():
-    st.markdown("zakon bagietek")
+    st.markdown("DevOps")
 
 
 def calculate_team():
+    data_analysis_ratio = 0
+    devops_ratio = 0
     answers = st.session_state["team answers"]
     if answers[1][0]:
-        show_team_1()
+        data_analysis_ratio += 1
     else:
-        show_team_2()
-
+        devops_ratio += 1
+    if answers[2][0]:
+        data_analysis_ratio += 1
+    else:
+        devops_ratio += 1
+    if answers[3][0]:
+        devops_ratio += 1
+    else:
+        data_analysis_ratio += 1
+    if answers[4][0]:
+        devops_ratio += 1
+    else:
+        data_analysis_ratio += 1
+    if answers[5][0]:
+        data_analysis_ratio += 1
+    else:
+        devops_ratio += 1
+    if answers[6][0]:
+        data_analysis_ratio += 1
+    else:
+        devops_ratio += 1
+    if answers[7][0] or answers[7][1]:
+        data_analysis_ratio += 1
+    else:
+        devops_ratio += 1
+    if data_analysis_ratio > devops_ratio:
+        st.write("")
+    else:
+        st.write("bardziej pasujesz do devopsa")
 
 def run():
     questions = load_questions("data/team_quiz_questions.yaml")
@@ -40,7 +69,6 @@ def run():
             st.experimental_rerun()
     else:
         calculate_team()
-        st.markdown(st.session_state["team answers"])
         if st.button("back to main page"):
             st.session_state['curr_view'] = 'init'
             st.session_state["team answers"] = {}
