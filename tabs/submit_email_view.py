f00e2e14524772b58e9fc16a084528170ea6dd1d
Mateusz Borowski <mateusz.borowski2.stud@pw.edu.pl>
2022-11-26 01:09:15 +0100

    Add some text

--- tabs/submit_email_view.py
+++ tabs/submit_email_view.py
@@ -2,12 +2,16 @@ import streamlit as st
 from scripts import send_email
 
 def run():
-    st.write("QL wynik ziomek")
+    st.header("Thank you for taking this quiz")
+
+    st.write("Jak chcesz wziąć udział w losowaniu, to daj mail")
 
     email = st.text_input("Your email", "email@example.com")
     
     if st.button("Submit"):
         send_email(email)
+
+    st.write("Chcesz z nami pogadać, a nie masz pomysłu jak zacząć, wylosuj pytanie")
 
     if st.button("Random Question"):
         st.session_state['curr_view'] = 'random_questions'
