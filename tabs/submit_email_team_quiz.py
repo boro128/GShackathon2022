565dcee3b129c6814a9a71b7b7232f1d9b1b2ab0
Szymon-Gut <72514490+Szymon-Gut@users.noreply.github.com>
2022-11-26 12:18:20 +0100

    team quiz refacotr

--- tabs/submit_email_team_quiz.py
+++ tabs/submit_email_team_quiz.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from tabs.utils import submit_mail_form
-
+from PIL import Image
 
 def calculate_team():
     data_analysis_ratio = 0
@@ -34,25 +34,29 @@ def calculate_team():
         data_analysis_ratio += 1
     else:
         devops_ratio += 1
-    if data_analysis_ratio > devops_ratio:
-        st.write("")
-    else:
-        st.write("bardziej pasujesz do devopsa")
-
-
-def show_team_1():
-    st.markdown("zakon chleba")
-
-
-def show_team_2():
-    st.markdown("zakon bagietek")
+    col1,col2 = st.columns(2)
+    with col1:
+        image = Image.open('resources/data_logo.png')
+        image = image.resize((150,150))
+        st.image(image)
+        st.write(f"Fitted to Data Analysis Team: `{round(data_analysis_ratio/7, 2)}`")
+        st.write("Data Analysis team take care about our Big Data problems, building predictive models, "
+                 "doing analysis raports and data visualizations. Some of the technologies used there is: Python, R, Hadoop")
+    with col2:
+        image = Image.open('resources/devops_logo.png')
+        image = image.resize((150, 150))
+        st.image(image)
+        st.write(f"\nFitted to DevOps Team: `{round(devops_ratio/7, 2)}`")
+        st.write("Our DevOps team is in charge of "
+                    "implementation the soutions build by our other teams. Some of the tools used in our DevOps team: "
+                    "Jenkins, Docker, Kubernetes, Terraform")
 
 
 def run():
 
     st.header(f"Thank you for taking this quiz.")
-
-    st.write("Jak chcesz maila, to klik")
+    calculate_team()
+    st.write("If You want to hear more about job opportunities in Goldman Sachs leave us Your email")
 
     submit_mail_form()
 
