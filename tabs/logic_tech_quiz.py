8dff54cb5a883c2c02217642788406ed7f46a49e
Mateusz Borowski <mateusz.borowski2.stud@pw.edu.pl>
2022-11-26 02:24:01 +0100

    Add time tracking

--- tabs/logic_tech_quiz.py
+++ tabs/logic_tech_quiz.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import time
 
 
 def run():
@@ -9,11 +10,12 @@ def run():
         options=(df.loc[st.session_state['question'], 'odp1'], df.loc[st.session_state['question'], 'odp2'],
                  df.loc[st.session_state['question'], 'odp3'])
     )
-    if st.session_state['question'] == 11:
+    if st.session_state['question'] == 10:
         if st.button("finish_quiz"):
+            end_time = time.time()
+            st.session_state['quiz_total_time'] = end_time - st.session_state['quiz_start_time']
             st.session_state['curr_view'] = 'submit_email'
             st.experimental_rerun()
-
-    if st.button("go to next"):
+    elif st.button("go to next"):
         st.session_state['question'] += 1
         st.experimental_rerun()
--- tabs/submit_email_view.py
+++ tabs/submit_email_view.py
@@ -5,6 +5,12 @@ from scripts import send_email
 def run():
     st.header("Thank you for taking this quiz")
 
+    time = st.session_state['quiz_total_time']
+    minutes = int(time // 60)
+    seconds = int(time % 60)
+
+    st.write(f"The quiz took you {minutes} {'minutes' if minutes != 1 else 'minute'} and {seconds} {'seconds' if seconds != 1 else 'second'}")
+
     st.write("Jak chcesz wziąć udział w losowaniu, to daj mail")
 
     email = st.text_input("Your email", "email@example.com")
